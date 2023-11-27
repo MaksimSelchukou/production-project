f98d1275fe31e31a2c34f6ce67b382c9d738d8c7
maksimselchukou <selchukouwebdev1995@gmail.com>
2023-11-27 16:30:38 +0300

    feat:added global css variables,added dark and light themes---

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React, {Suspense} from 'react';
-import './styles/index.scss'
 import {Link, Route, Routes} from 'react-router-dom';
 import {MainPageAsync} from "./pages/mainPage/mainPage.async";
 import {AboutPageAsync} from "./pages/aboutPage/AboutPage.async";
-import {useTheme} from "./theme/useTheme";
-
 
 const App = () => {
-    const {theme, toggleTheme } = useTheme()
-    console.log('app перерисовывается')
     return (
-        <div className={`app ${theme}`}>
-            <button onClick={toggleTheme}>toggle theme</button>
+        <div className='app'>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About me</Link>
             <Suspense fallback={<div>...LOADING</div>}>
--- src/pages/aboutPage/AboutPage.async.tsx
+++ src/pages/aboutPage/AboutPage.async.tsx
@@ -3,7 +3,6 @@ import {lazy} from "react";
 export const AboutPageAsync = lazy(() => new Promise((res) => {
     setTimeout(() => {
         // @ts-ignore
-        //задержка сделана для наглядости,так делать не будем
         res(import('./AboutPage'))
     }, 1000)
 }));
